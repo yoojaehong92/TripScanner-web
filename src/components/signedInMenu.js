b8ac515bf19e1dc84e0cd3ae11f345d7d76af9eb
Yoo Jaehong <hibria21@gmail.com>
2017-05-24 20:26:59 +0900

    프로필 이미지 업로드 구현

--- src/components/signedInMenu.js
+++ src/components/signedInMenu.js
@@ -4,7 +4,9 @@ import 'whatwg-fetch';
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
+import { ListItem } from 'material-ui/List'
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import Avatar from 'material-ui/Avatar';
 
 import PropTypes from 'prop-types'
 import { fetchSignOut } from '../actions/userAction'
@@ -13,7 +15,8 @@ import { push } from 'react-router-redux';
 
 class SignedInMenu extends React.Component {
   static propTypes = {
-    dispatch: PropTypes.func.isRequired
+    dispatch: PropTypes.func.isRequired,
+    user: PropTypes.object
   };
 
   constructor(props) {
@@ -22,27 +25,38 @@ class SignedInMenu extends React.Component {
 
   render() {
     return (
-      <IconMenu
-        iconButtonElement={
-          <IconButton>
-            <MoreVertIcon color="white"/>
-          </IconButton>
-        }
-        targetOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-      >
-        <MenuItem primaryText="Refresh" />
-        <MenuItem
-          primaryText="Edit Profile"
-          onTouchTap={ () => this.props.dispatch(push('/profile_edit')) }
+        <ListItem
+          primaryText={ this.props.user.email }
+          leftAvatar={<Avatar src={ this.props.user.image_thumb }/>}
+          rightIconButton={
+            <IconMenu
+              iconButtonElement={
+                <IconButton>
+                  <MoreVertIcon color="white"/>
+                </IconButton>
+              }
+              targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+              anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+            >
+              <MenuItem primaryText="Refresh" />
+              <MenuItem
+                primaryText="Edit Profile"
+                onTouchTap={ () => this.props.dispatch(push('/profile_edit')) }
+              />
+              <MenuItem
+                primaryText="Sign out"
+                onTouchTap={ () => this.props.dispatch(fetchSignOut()) }
+              />
+            </IconMenu>
+          }
+          disabled
         />
-        <MenuItem
-          primaryText="Sign out"
-          onTouchTap={ () => this.props.dispatch(fetchSignOut()) }
-        />
-      </IconMenu>
     )
   }
 }
-
-export default connect()(SignedInMenu);
+function mapCurrentUser(state) {
+  return {
+    user: state.currentUserReducer.user
+  };
+}
+export default connect(mapCurrentUser)(SignedInMenu);
